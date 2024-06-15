3379d7f880d191838c30249b227778211d55ce26
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-06-15 17:32:40 +0300

    mini-fix 1

---
+++ VoiceRecorder/Filters/DistortionFilter.cs
@@ -0,0 +1,20 @@
+﻿using CSCore;
+using CSCore.Streams.Effects;
+using VoiceRecorder.Filters.Interfaces;
+
+namespace VoiceRecorder.Filters;
+
+public class CompressorFilter : IAudioFilter
+{
+    public IWaveSource ApplyFilter(IWaveSource source)
+    {
+        var compressor = new DmoCompressorEffect(source);
+        compressor.Attack = 10;
+        compressor.Gain = 15;
+        compressor.Predelay = 4;
+        compressor.Release = 200;
+        compressor.Threshold = -20;
+        compressor.Ratio = 3;
+        return compressor;
+    }
+}
--- VoiceRecorder/Filters/FlangerFilter.cs
+++ VoiceRecorder/Filters/FlangerFilter.cs
@@ -1,13 +1,15 @@
 ﻿using CSCore;
+using CSCore.DMO.Effects;
 using CSCore.Streams.Effects;
 using VoiceRecorder.Filters.Interfaces;
 
 namespace VoiceRecorder.Filters;
 
-public class CompressorFilter : IAudioFilter
+public class FlangerFilter : IAudioFilter
 {
     public IWaveSource ApplyFilter(IWaveSource source)
     {
-        return new DmoCompressorEffect(source);
+        return new DmoFlangerEffect(source);
     }
 }
+
